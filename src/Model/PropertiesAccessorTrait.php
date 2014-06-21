2e01641c003fb0dedb0208c8b8634370898ea455
Ivan Kerin <ikerin@gmail.com>
2014-06-22 02:26:20 +0300

    scrutinizer suggestion

--- src/Model/PropertiesAccessorTrait.php
+++ src/Model/PropertiesAccessorTrait.php
@@ -12,7 +12,7 @@ namespace Harp\Core\Model;
 trait PropertiesAccessorTrait
 {
     /**
-     * @param object $object
+     * @param PropertiesAccessorTrait $object
      * @return array
      */
     public static function getPublicPropertiesOf($object)
